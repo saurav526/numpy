2a91c8e6f40835ca37fdf99ac8d76274f450ab08
saurav526 <sauravtiwari2712005@gmail.com>
2026-01-16 22:11:41 +0530

    zeros ones empty ,eye, linsppace  implementation

---
+++ Array Creation Functions/ones_zero_empty_etc.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+array = np.array([[1, 2, 3], [4, 5, 6]])
+print("Original 2D Array:")
+print(array)
+print("Original 1D Array:")
+
+array = np.array([1, 2, 3])
+print(array)
+
+print("----- ARRAY CREATION FUNCTIONS -----")
+
+# 1. array()
+a = np.array([1, 2, 3, 4, 5])
+print("np.array():", a)
+
+# 2. zeros()
+b = np.zeros((2, 3))
+print("\nnp.zeros():\n", b)
+
+# 3. ones()
+c = np.ones((3, 2))
+print("\nnp.ones():\n", c)
+
+# 4. empty()
+d = np.empty((2, 2))
+print("\nnp.empty():\n", d)
+
+# 5. arange()
+e = np.arange(1, 10, 2)
+print("\nnp.arange():", e)
+
+# 6. linspace()
+f = np.linspace(1, 10, 5)
+print("\nnp.linspace():", f)
+
+# 7. full()
+g = np.full((2, 3), 7)
+print("\nnp.full():\n", g)
+
+# 8. eye()
+h = np.eye(3)
+print("\nnp.eye():\n", h) 
+
+h = np.eye(10) 
+print("\nnp.eye():\n", h) 
+
+h = np.eye(3 , 5)
+print("\nnp.eye():\n", h) 
+
+h = np.eye(6 , 4 , )
+print("\nnp.eye():\n", h) 
+
+h = np.eye(6 , 4 , k = 1)
+print("\nnp.eye():\n", h) 
